71f860a6a0a12765a5d577b8d3b0ca38b23971fb
unknown <urkov522@gmail.com>
2026-09-21 13:52:04 +0300

    привіт

---
+++ Hello/Hello.cpp
@@ -0,0 +1,12 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World! :)" << endl;
+    system("chcp 1251 > NUL");
+    cout << "Привіт, світ! :)" << endl;
+}
+
+
